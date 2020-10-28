e3e8f4317b7c3252b3212db324efb40b41bdbf08
M. Gao <gao.myue@gmail.com>
2020-10-28 11:09:55 +0100

    improvement: stage changes

---
+++ odv/renderarea.cpp
@@ -0,0 +1,341 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include "renderarea.h"
+#include "carla/Memory.h"
+#include "carla/geom/Vector2D.h"
+#include "carla/road/element/Geometry.h"
+#include "carla/road/element/RoadInfoGeometry.h"
+#include "carla/road/element/RoadInfoVisitor.h"
+
+#include <QtGui/QPainter>
+#include <QtGui/QPainterPath>
+#include <qnamespace.h>
+
+using namespace carla;
+using namespace carla::road;
+
+//! [0]
+RenderArea::RenderArea(const carla::road::Map& map, QWidget *parent)
+    : QWidget(parent),
+      map_(map),
+      mesh_(map_.GenerateMesh(10.))
+{
+    shape = Polygon;
+    antialiased = false;
+    transformed = false;
+    pixmap.load(":/images/qt-logo.png");
+
+    QPalette pal;
+    pal.setColor(QPalette::Background, Qt::gray);
+    // setBackgroundRole(QPalette::Base);
+    setAutoFillBackground(true);
+    setPalette(pal);
+}
+//! [0]
+
+//! [1]
+QSize RenderArea::minimumSizeHint() const
+{
+    return QSize(100, 100);
+}
+//! [1]
+
+//! [2]
+QSize RenderArea::sizeHint() const
+{
+    return QSize(1920, 1680);
+}
+//! [2]
+
+//! [3]
+void RenderArea::setShape(Shape shape)
+{
+    this->shape = shape;
+    update();
+}
+//! [3]
+
+//! [4]
+void RenderArea::setPen(const QPen &pen)
+{
+    this->pen = pen;
+    update();
+}
+//! [4]
+
+//! [5]
+void RenderArea::setBrush(const QBrush &brush)
+{
+    this->brush = brush;
+    update();
+}
+//! [5]
+
+//! [6]
+void RenderArea::setAntialiased(bool antialiased)
+{
+    this->antialiased = antialiased;
+    update();
+}
+//! [6]
+
+//! [7]
+void RenderArea::setTransformed(bool transformed)
+{
+    this->transformed = transformed;
+    update();
+}
+//! [7]
+
+//! [8]
+void RenderArea::paintEvent(QPaintEvent * /* event */)
+{
+    //static const QPoint points[4] = {
+    //    QPoint(10, 80),
+    //    QPoint(20, 10),
+    //    QPoint(80, 30),
+    //    QPoint(90, 70)
+    //};
+    //
+
+    // QRect rect(10, 20, 80, 60);
+
+    // QPainterPath path;
+    // path.moveTo(20, 80);
+    // path.lineTo(20, 30);
+    // path.cubicTo(80, 0, 50, 50, 80, 80);
+
+    // int startAngle = 20 * 16;
+    // int arcLength = 120 * 16;
+//! [8]
+
+//! [9]
+    QPainter painter(this);
+    painter.setPen(pen);
+    painter.setBrush(Qt::BrushStyle::SolidPattern);
+    if (antialiased)
+        painter.setRenderHint(QPainter::Antialiasing, true);
+//! [9]
+
+//! [10]
+    auto& vs = mesh_.GetVertices();
+    auto n = vs.size();
+
+    auto& odr = map_.GetMap();
+    auto& roads = odr.GetRoads();
+
+    painter.save();
+    // painter.setBrush(Qt::SolidPattern);
+    painter.translate(500, 500);
+    // painter.scale(0.3, 0.3);
+    painter.scale(3., 3.);
+
+    for (size_t i = 0; i < n - 2; ++i)
+    {
+      const QPoint points3[3] = {QPoint(vs[i].x, vs[i].y),
+                                 QPoint(vs[i + 1].x, vs[i + 1].y),
+                                 QPoint(vs[i + 2].x, vs[i + 2].y)};
+      painter.drawPolygon(points3, 3);
+    }
+
+    painter.setPen(QPen(Qt::white));
+    for (auto& r : roads)
+    {
+      if (r.first != 508)
+        continue;
+
+      auto geos = r.second.GetInfos<element::RoadInfoGeometry>();
+
+      // for (auto &geo : geos) {
+      //   std::cout << r.first << ": " 
+      //             << " type=" << (int)geo->GetGeometry().GetType() 
+      //             << " s=" << geo->GetDistance()
+      //             << " len=" << geo->GetGeometry().GetLength()
+      //             << " start_offset=" << geo->GetGeometry().GetStartOffset()
+      //             << std::endl;
+      // }
+      auto l_sections = r.second.GetLaneSections();
+
+      for (auto& ls : l_sections)
+      {
+        auto& lanes = ls.GetLanes();
+        for (auto& l : lanes)
+        {
+          auto rmarks = l.second.GetInfos<element::RoadInfoMarkRecord>();
+          for (auto& rm : rmarks)
+          {
+            std::cout << r.first << "/" << l.first << ": " 
+                      << " s=" << ls.GetDistance()
+                      << " len=" << l.second.GetLength()
+                      << " type=" << rm->GetType()
+                      << " typename=" << rm->GetTypeName()
+                      << " width=" << rm->GetWidth()
+                      << " height=" << rm->GetHeight()
+                      << " color=" << rm->GetColor()
+                      << " distance=" << rm->GetDistance()
+                      << " lc=" << (int)rm->GetLaneChange()
+                      << " material=" << rm->GetMaterial()
+                      << " id=" << rm->GetRoadMarkId()
+                      << " weight=" << rm->GetWeight()
+                      << std::endl;
+            for (auto &geo : geos)
+            {
+              if (rm->GetType() == "none")
+                continue;
+
+              if (geo->GetGeometry().GetType() == element::GeometryType::LINE) {
+                const element::GeometryLine *geo_line =
+                    dynamic_cast<const element::GeometryLine *>(
+                        &geo->GetGeometry());
+                auto start_s = geo_line->GetStartOffset();
+                auto end_s = start_s + geo_line->GetLength();
+                auto start_p = geo_line->PosFromDist(start_s);
+                auto end_p = geo_line->PosFromDist(end_s);
+                std::cout << "geo: "
+                          << " start_s=" << geo_line->GetStartOffset()
+                          << " length=" << geo_line->GetLength()
+                          << " hdg=" << geo_line->GetHeading()
+                          << " start_p=" << start_p.location.x << ", "
+                          << start_p.location.y
+                          << " endl_p=" << end_p.location.x << ", "
+                          << end_p.location.y << std::endl;
+                auto yaw = geo_line->GetHeading();
+                geom::Vector2D dv(-std::sin(yaw), std::cos(yaw));
+                //auto width = l.second.GetWidth(0.);
+                auto width = 3.5;
+                geom::Vector2D offset(dv * l.first);
+                std::cout << l.first << ": " << width << std::endl;
+                geom::Vector2D s_offset(
+                    geom::Vector2D(start_p.location.x, start_p.location.y) +
+                    offset * 3.5);
+                geom::Vector2D e_offset(
+                    geom::Vector2D(end_p.location.x, end_p.location.y) +
+                    offset * 3.5);
+                painter.drawLine(s_offset.x, s_offset.y, 
+                                 e_offset.x, e_offset.y);
+              }
+            }
+          }
+        }
+      }
+    }
+    painter.restore();
+    /*
+    for (int x = 0; x < width(); x += 100) {
+        for (int y = 0; y < height(); y += 100) {
+            painter.save();
+            painter.translate(x, y);
+//! [10] //! [11]
+            if (transformed) {
+                painter.translate(50, 50);
+                painter.rotate(60.0);
+                painter.scale(0.6, 0.9);
+                painter.translate(-50, -50);
+            }
+//! [11]
+
+//! [12]
+            switch (shape) {
+            case Line:
+                painter.drawLine(rect.bottomLeft(), rect.topRight());
+                break;
+            case Points:
+                painter.drawPoints(points, 4);
+                break;
+            case Polyline:
+                painter.drawPolyline(points, 4);
+                break;
+            case Polygon:
+                painter.drawPolygon(points, 3);
+                painter.drawPolygon(points2, 3);
+                painter.drawPolygon(points3, 3);
+                break;
+            case Rect:
+                painter.drawRect(rect);
+                break;
+            case RoundedRect:
+                painter.drawRoundedRect(rect, 25, 25, Qt::RelativeSize);
+                break;
+            case Ellipse:
+                painter.drawEllipse(rect);
+                break;
+            case Arc:
+                painter.drawArc(rect, startAngle, arcLength);
+                break;
+            case Chord:
+                painter.drawChord(rect, startAngle, arcLength);
+                break;
+            case Pie:
+                painter.drawPie(rect, startAngle, arcLength);
+                break;
+            case Path:
+                painter.drawPath(path);
+                break;
+            case Text:
+                painter.drawText(rect,
+                                 Qt::AlignCenter,
+                                 tr("Qt by\nThe Qt Company"));
+                break;
+            case Pixmap:
+                painter.drawPixmap(10, 10, pixmap);
+            }
+//! [12] //! [13]
+            painter.restore();
+        }
+    }
+    */
+
+    painter.setRenderHint(QPainter::Antialiasing, false);
+    painter.setPen(palette().dark().color());
+    painter.setBrush(Qt::NoBrush);
+    painter.drawRect(QRect(0, 0, width() - 1, height() - 1));
+}
+//! [13]
